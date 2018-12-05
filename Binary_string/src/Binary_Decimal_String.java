9c0b5db5ba25e92c91e704d80cfdec81f93098db
TaressaHalpin <35612541+TaressaHalpin@users.noreply.github.com>
2018-12-05 16:04:39 -0500

    Add files via upload

---
+++ Binary_string/src/Binary_Decimal_String.java
@@ -0,0 +1,27 @@
+
+
+
+public class Binary_Decimal_String 
+{
+   public static void main(String a[])
+   {
+	   System.out.println("Binary representation of 382: ");
+	   System.out.println(Integer.toBinaryString(1382));
+	   System.out.println("\nBinary representation of 25: ");
+	   System.out.println(Integer.toBinaryString(25));
+	   System.out.println("\nBinary representation of 989");
+	   System.out.println(Integer.toBinaryString(989));
+	   
+	   
+	   
+	   
+   }
+	
+	
+	
+}
+
+
+
+
+
